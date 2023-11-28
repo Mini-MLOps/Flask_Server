ce16d75aea316fff35b0d408bf3e769b36d21179
soyeonii <dingdingcrong@naver.com>
2023-11-28 17:26:10 +0900

    mecab 수정

--- MeCab_tokenizer.py
+++ MeCab_tokenizer.py
@@ -1,9 +1,9 @@
-from konlpy.tag import MeCab
+import MeCab
 
 
 class MeCab_tokenizer:
     def __init__(self):
-        self.mecab = MeCab()
+        self.mecab = MeCab.Tagger()
 
     def tokenize(self, text):
         mecab_tokens = self.mecab.parse(text).split()
