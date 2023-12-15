431f3d1d3a59f29270e6f5d2cf427c68c31e69d7
soyeonii <dingdingcrong@naver.com>
2023-12-15 11:06:27 +0900

    fix: DB 연결 시 매번 재접속 하도록 수정

--- DB_connect.py
+++ DB_connect.py
@@ -3,6 +3,9 @@ import pymysql
 
 class DB_connect:
     def __init__(self):
+        self.connect()
+
+    def connect(self):
         self.conn = pymysql.connect(
             host="211.62.99.58",
             port=3326,
@@ -11,22 +14,26 @@ class DB_connect:
             db="mlops_db",
             charset="utf8",
         )
-        self.conn.ping(reconnect=True)
         self.curs = self.conn.cursor()
 
     def select(self, sql):
+        self.connect()
         self.curs.execute(sql)
         data = self.curs.fetchall()
+        self.close()
         return data
 
     def insert(self, sql, data):
+        self.connect()
         self.curs.execute(sql, data)
         self.conn.commit()
+        self.close()
 
     def truncate(self, table_name):
-        truncate_query = f"TRUNCATE {table_name};"
-        self.curs.execute(truncate_query)
+        self.connect()
+        self.curs.execute(f"TRUNCATE {table_name};")
         self.conn.commit()
+        self.close()
 
     def close(self):
         self.curs.close()
