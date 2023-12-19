65fbebcabbf88484cf3f3a146ef0a41fdf843000
soyeonii <dingdingcrong@naver.com>
2023-12-19 12:47:07 +0900

    fix: GPT 임베딩 모델 설정 수정

--- GPT_embedding.py
+++ GPT_embedding.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import os
 import openai
 
 
 class GPT_embedding:
-    def __init__(self, api_key, model):
-        openai.api_key = api_key
-        self.model = model
+    def __init__(self):
+        api_version = "2023-03-15-preview"
+
+        os.environ["AZURE_OPENAI_ENDPOINT"] = "https://student-oai.openai.azure.com/"
+        os.environ["AZURE_OPENAI_API_KEY"] = "8ff210f0e95f4a00aa652b331f1b9c28"
+
+        openai.api_type = "azure"
+        openai.api_version = api_version
 
     def vectorize(self, text):
-        return openai.Embedding.create(model=self.model, input=text).data[0].embedding
+        return openai.embeddings.create(model="text-embedding-ada-002", input=text).data[0].embedding
